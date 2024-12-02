b8a76eb9e30a276cbd764aad5ae4e5fa00949e1d
Jean Vasquez <jeanpool5921@gmail.com>
2024-12-02 01:33:05 -0500

    Treaer Nuevos Productos

--- src/app/home/components/explorer-product/explorer-product.component.ts
+++ src/app/home/components/explorer-product/explorer-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NewProduts } from '../../../products/interfaces/new-product';
+import { ProductsService } from '../../../products/services/Products.service';
 
 @Component({
   selector: 'home-explorer-product',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './explorer-product.component.css'
 })
 export class ExplorerCategoriesComponent {
+
+  products: NewProduts[] = [];
+  constructor(private productsService: ProductsService){}
+
+  ngOnInit(): void {
+    this.loadProductos();
+  }
+  
+  async loadProductos() {
+    const products = await this.productsService.newAllProducts();
+    this.products = products ?? [];
+  }
 
 }
